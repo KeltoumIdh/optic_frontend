023465e1ca593bf2d24b40ad62e465284442dc01
keltoum <keltoumidhssou5wp@gmail.com>
2024-05-11 16:20:00 +0100

    call csrf() method after all requests

--- src/services/Api/Clients/ClientApi.jsx
+++ src/services/Api/Clients/ClientApi.jsx
@@ -1,22 +1,30 @@
 import {axiosClient} from "../../../api/axios";
 
+const csrf = () => axiosClient.get("/sanctum/csrf-cookie");
+
 const ClientApi = {
     delete: async (id) => {
+        await csrf();
         return await axiosClient.delete(`/clients/delete/${id}`)
     },
     show: async (id) => {
+        await csrf();
         return await axiosClient.get(`/clients/details/${id}`);
     },
     update: async (id, payload) => {
+        await csrf();
         return await axiosClient.post(`/clients/update/${id}`, payload)
     },
     edit: async (id, payload) => {
+        await csrf();
         return await axiosClient.post(`/clients/edit/${id}`, payload)
     },
     create: async (payload) => {
+        await csrf();
         return await axiosClient.post('/clients/add', payload)
     },
     all: async () => {
+        await csrf();
         // return await axiosClient.get('/clients')
         try {
         const response = await axiosClient.get('/clients');
