b00aef2a08c741c1f692eea80b635eebcaeac79e
keltoum <keltoumidhssou5wp@gmail.com>
2024-06-21 01:10:57 +0100

    change baseUrl for axios methods

--- src/api/axiosClient.jsx
+++ src/api/axiosClient.jsx
@@ -1,16 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Create an axios instance
 const axiosClient = axios.create({
-    baseURL: "http://localhost:8000",
-    headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-    },
-    withCredentials: true,
-    withXSRFToken: true,
+  baseURL: "https://tyfwt-vision.website",
+  headers: {
+    "X-Requested-With": "XMLHttpRequest",
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+  withXSRFToken: true,
 });
-
 
 export default axiosClient;
