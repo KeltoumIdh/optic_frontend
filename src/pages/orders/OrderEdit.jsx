68a6bf11d5fb6d0e55c387e09764f7122ce93748
hixvm <hixvmx@gmail.com>
2024-07-10 09:00:10 +0100

    fix(/orders/edit/{id}): convert selected files to base64 + hide submit BTN on sending

--- src/pages/orders/OrderEdit.jsx
+++ src/pages/orders/OrderEdit.jsx
@@ -19,6 +19,7 @@ import { Label } from "../../components/ui/label.jsx";
 import SuccessPopup from "../../components/Popups/SuccessPopup.jsx";
 import { useAuth } from "@/hooks/useAuth.jsx";
 import axiosClient from "@/api/axiosClient.jsx";
+import Spinner from "@/components/Spinner.jsx";
 
 
 const formSchema = z.object({
@@ -27,7 +28,7 @@ const formSchema = z.object({
   paid_price: z.string().max(50),
   remain_price: z.number().min(0),
   total_price: z.number().min(0).optional(),
-  payement_file: z.string().max(50).nullable(),
+  // payement_file: z.string().max(50).nullable(),
 });
 
 export default function OrderEdit() {
@@ -46,9 +47,11 @@ export default function OrderEdit() {
 
   const { handleSubmit, register, setValue, formState } = form;
 
+  const [isLoading, setIsLoading] = useState(false)
   useEffect(() => {
     const fetchOrder = async () => {
       try {
+        setIsLoading(true)
         await csrf();
         const response = await axiosClient.post(`/api/orders/edit/${id}`);
         setValue("payment_method", response.data.payment_method);
@@ -56,26 +59,47 @@ export default function OrderEdit() {
         setValue("paid_price", response.data.paid_price);
         setValue("remain_price", response.data.remain_price);
         setValue("total_price", response.data.total_price);
-        setValue("payement_file", response.data.payement_file);
+        // setValue("payement_file", response.data.payement_file);
         setIsCredit(response.data.is_credit);
       } catch (error) {
         console.error("Fetch Order Error", error);
+      } finally {
+        setIsLoading(false)
       }
     };
     fetchOrder();
   }, [id, setValue]);
 
+
+  const [file, setFile] = useState("")
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setFile(reader.result); // Base64 string
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setFile('')
+    }
+  }
+
+
+  const [inProgress, setInProgress] = useState(false)
   const onSubmit = async (e) => {
     e.preventDefault();
 
     const data = {
       paid_price: e?.target?.paid_price?.value,
       payment_method: e?.target?.payment_method?.value,
-      payement_file: e?.target?.payement_file?.value,
+      payement_file: file,
       date_fin_credit: e?.target?.date_fin_credit?.value ?? "",
     };
-    
+
     try {
+      setInProgress(true)
       await csrf();
       const response = await axiosClient.put(`/api/orders/update/${id}`, data);
       toast({
@@ -91,6 +115,8 @@ export default function OrderEdit() {
         description: "Failed to update order.",
         status: "error",
       });
+    } finally {
+      setInProgress(false)
     }
   };
 
@@ -98,7 +124,7 @@ export default function OrderEdit() {
     setShowSuccessPopup(false);
   };
 
-  return (
+  return isLoading ? <Spinner/> : (
     <>
       <div className="flex items-center p-2">
         <Link to={"/orders"} className="mr-2">
@@ -176,7 +202,8 @@ export default function OrderEdit() {
               id="payement_file"
               name="payement_file"
               type="file"
-              {...register("payement_file")}
+              onChange={handleFileChange}
+              accept="image/png, image/jpg, image/jpeg"
             />
           </div>
           <FormField
@@ -193,11 +220,8 @@ export default function OrderEdit() {
             )}
           />
 
-          <Button type="submit" disabled={formState.isSubmitting}>
-            {formState.isSubmitting && (
-              <Loader className={"mx-2 my-2 animate-spin"} />
-            )}
-            wa Modifier
+          <Button type="submit" disabled={inProgress}>
+            {!inProgress ? 'Modifier' : 'Loading...'}
           </Button>
         </form>
       </Form>
