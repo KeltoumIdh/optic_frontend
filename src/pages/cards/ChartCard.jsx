05899689934beef79a29660731aedbe61ee0191c
hixvm <hixvmx@gmail.com>
2024-07-20 15:53:38 +0100

    fix error, show loader in `clients/add`

--- src/pages/cards/ChartCard.jsx
+++ src/pages/cards/ChartCard.jsx
@@ -18,7 +18,7 @@ export default function ChartCard() {
                 setisLoading(true) 
                 await csrf();
                 const response = await axiosClient.get('/api/top/product');
-                setProducts(response.data);
+                setProducts(response?.data || []);
             } catch (error) {
                 console.error('Erreur lors de la récupération des produits les plus vendus :', error);
             } finally {
@@ -30,18 +30,19 @@ export default function ChartCard() {
     }, []);
 
     // Extraire les noms des produits et les quantités vendues
-    const productNames = products.map(product => product.name);
-    const quantitiesSold = products.map(product => product.quantity_sold);
+    const productNames = products?.map(product => product.name);
+    const quantitiesSold = products?.map(product => product.quantity_sold);
 
     return isLoading ? <div className="h-72 w-100 rounded-md border animate-pulse"></div> : (
         <Card className="h-[100%] w-[100%]">
             <h4 className="mb-4 text-lg font-medium leading-none p-4 ">Produit plus vende</h4>
+            {products.length > 0 &&
             <BarChart
                 series={[{ data: quantitiesSold }]}
                 height={290}
                 xAxis={[{ data: productNames, scaleType: 'band' }]}
                 margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
-            />
+            />}
         </Card>
     );
 }
