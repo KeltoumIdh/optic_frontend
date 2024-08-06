a09e612232dbd35a291894c8b64d839fa80aeba2
keltoum <keltoumidhssou5wp@gmail.com>
2024-08-06 20:40:44 +0100

    change the condirmation text

--- src/pages/cards/listCard.jsx
+++ src/pages/cards/listCard.jsx
@@ -1,8 +1,8 @@
-import * as React from "react"
-import { ScrollArea } from "@/components/ui/scroll-area"
-import { Separator } from "@/components/ui/separator"
-import { Link } from "react-router-dom"
-import { axiosUser } from "../../api/axios"
+import * as React from "react";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
+import { Link } from "react-router-dom";
+import { axiosUser } from "../../api/axios";
 import {
   Dialog,
   DialogContent,
@@ -11,25 +11,22 @@ import {
   DialogTitle,
   DialogTrigger,
   DialogFooter,
-  DialogClose
-} from "@/components/ui/dialog"
-import { Button } from "../../components/ui/button"
-import { useAuth } from "@/hooks/useAuth"
+  DialogClose,
+} from "@/components/ui/dialog";
+import { Button } from "../../components/ui/button";
+import { useAuth } from "@/hooks/useAuth";
 import axiosClient from "@/api/axiosClient.jsx";
 
-
-
 export function ListCard() {
-
   const { csrf } = useAuth();
 
-  const [clients, setClients] = React.useState([])
+  const [clients, setClients] = React.useState([]);
 
-  const [isLoading, setisLoading] = React.useState(false)
+  const [isLoading, setisLoading] = React.useState(false);
 
   const fetchClients = async () => {
     try {
-      setisLoading(true)
+      setisLoading(true);
       await csrf();
       const response = await axiosClient.get("/api/credit/clients");
       if (response.status === 200) {
@@ -41,7 +38,7 @@ export function ListCard() {
     } catch (error) {
       console.error("Error fetching clients:", error);
     } finally {
-      setisLoading(false)
+      setisLoading(false);
     }
   };
 
@@ -52,7 +49,9 @@ export function ListCard() {
   const getStatusColorClass = (client) => {
     const currentDate = new Date();
     const finCreditDate = new Date(client.date_fin_credit);
-    const daysRemaining = Math.ceil((finCreditDate - currentDate) / (1000 * 3600 * 24));
+    const daysRemaining = Math.ceil(
+      (finCreditDate - currentDate) / (1000 * 3600 * 24)
+    );
 
     if (daysRemaining < 0) {
       return "bg-red-600 text-white";
@@ -63,28 +62,35 @@ export function ListCard() {
     }
   };
 
-  const [confirmationInProgress, setConfirmationInProgress] = React.useState(false)
+  const [confirmationInProgress, setConfirmationInProgress] =
+    React.useState(false);
   const sendConfirmationRequest = async (id) => {
     try {
-      setConfirmationInProgress(true)
+      setConfirmationInProgress(true);
       await csrf();
-      const response = await axiosClient.put("/api/confirmOrder", { order_id: id });
+      const response = await axiosClient.put("/api/confirmOrder", {
+        order_id: id,
+      });
       if (response.status === 200) {
-        fetchClients()
+        fetchClients();
       } else {
         throw new Error("Failed to fetch clients");
       }
     } catch (error) {
       console.error("Error fetching clients:", error);
     } finally {
-      setConfirmationInProgress(false)
+      setConfirmationInProgress(false);
     }
-  }
+  };
 
-  return isLoading ? <div className="h-72 w-100 rounded-md border animate-pulse"></div> : (
+  return isLoading ? (
+    <div className="h-72 w-100 rounded-md border animate-pulse"></div>
+  ) : (
     <ScrollArea className="h-72 w-100 rounded-md border">
       <div className="p-4">
-        <h4 className="mb-4 text-lg font-medium leading-none py-4">Clients crédit</h4>
+        <h4 className="mb-4 text-lg font-medium leading-none py-4">
+          Clients crédit
+        </h4>
         <div className="flex flex-row justify-between md:px-3">
           <div className="text-sm font-medium">Client</div>
           <div className="text-sm font-medium">Téléphone</div>
@@ -96,28 +102,53 @@ export function ListCard() {
         {clients.map((client) => {
           const currentDate = new Date();
           const finCreditDate = new Date(client.date_fin_credit);
-          const daysRemaining = Math.ceil((finCreditDate - currentDate) / (1000 * 3600 * 24));
+          const daysRemaining = Math.ceil(
+            (finCreditDate - currentDate) / (1000 * 3600 * 24)
+          );
           return (
             <div className="">
-              <div key={client.id} className='flex flex-row justify-between  px-2'>
-
-                <div className="text-sm">{client.name} {client.lname}</div>
+              <div
+                key={client.id}
+                className="flex flex-row justify-between  px-2"
+              >
+                <div className="text-sm">
+                  {client.name} {client.lname}
+                </div>
                 <div className="text-sm">{client.phone}</div>
                 <div className="text-sm">{client.remain_price}</div>
-                <div className='text-sm '><span className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full w-600 ${getStatusColorClass(client)}`}> {daysRemaining} jours</span></div>
+                <div className="text-sm ">
+                  <span
+                    className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full w-600 ${getStatusColorClass(
+                      client
+                    )}`}
+                  >
+                    {" "}
+                    {daysRemaining} jours
+                  </span>
+                </div>
                 <div>
                   <Dialog>
                     <DialogTrigger>Confirmer</DialogTrigger>
                     <DialogContent>
                       <DialogHeader>
-                        <DialogTitle>Modifier order </DialogTitle>
+                        <DialogTitle>
+                          Modifier la Commande de {client.name} {client.lname}
+                        </DialogTitle>
                         <DialogDescription>
-                          This action cannot be undone. Are you sure you want to permanently
-                          delete this file from our servers?
+                          prix reste {client.remain_price}
+                        </DialogDescription>
+                        <DialogDescription>
+                          Cette action ne peut pas être annulée. Êtes-vous sûr
+                          de vouloir confirmer définitivement le payement de cette commande ?
                         </DialogDescription>
                       </DialogHeader>
                       <DialogFooter>
-                        <Button type="submit" onClick={() => sendConfirmationRequest(client.id)}>{confirmationInProgress ? 'Loading...' : 'Confirm'}</Button>
+                        <Button
+                          type="submit"
+                          onClick={() => sendConfirmationRequest(client.id)}
+                        >
+                          {confirmationInProgress ? "Loading..." : "Confirm"}
+                        </Button>
                         <DialogClose asChild>
                           <Button type="button" variant="secondary">
                             Close
